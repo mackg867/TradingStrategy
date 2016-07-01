fff92beb77ea5c2ed99b84bee30098e4d5a5202d
mackg867 <mackenzie.ginithan@gmail.com>
2016-06-30 19:49:47 -0700

    Add files via upload

---
+++ ShortUVXYSensitivityAnalysis.R
@@ -0,0 +1,137 @@
+########
+# VIX/VXV Ratio Filter for UVXY Parameter Sensetivity Analysis
+#########
+library(quantmod)
+getSymbols("^VIX",src="yahoo")
+getSymbols("^VXV",src="yahoo")
+getSymbols("VXX",src="yahoo")
+getSymbols("UVXY",src="yahoo")
+VXV = VXV[-1794,] #For some reason the data includes President's Day in 2014
+
+close = as.numeric(UVXY[,6])
+open = ((as.numeric(UVXY[,1])-as.numeric(UVXY[,4]))/as.numeric(UVXY[,4]))*close + close
+high = ((as.numeric(UVXY[,2])-as.numeric(UVXY[,4]))/as.numeric(UVXY[,4]))*close + close
+low = ((as.numeric(UVXY[,3])-as.numeric(UVXY[,4]))/as.numeric(UVXY[,4]))*close + close
+vxv = as.numeric(get("VXV")[1199:nrow(VXV),6])
+vix = as.numeric(get("VIX")[1199:nrow(VIX),6])
+
+trials = 50000
+slVec = seq(.38,.54,.005)
+ptVec = seq(.52,.66,.005)
+rptVec = seq(.25,.35,.01)
+triggerVec = seq(.95,1.01,.005)
+
+output = matrix(rep(NA,trials)*5,nrow=trials,ncol=5)
+
+for(z in 1:trials){
+
+slip = 0
+sl = sample(slVec,1)
+pt = sample(ptVec,1)
+rpt = sample(rptVec,1)
+trigger = sample(triggerVec,1)
+  
+inTrade = 0
+enterPrice = 0
+exitPrice = 0
+enter = 0
+stopPrice = 0
+profitPrice = 0
+returns = c()
+block = 0
+
+
+runEquity = rep(-1000,length(close))
+runEquity[1] = 100000
+saveEquity = 0
+nShares = 0
+
+enterPrices = c()
+enterDays = c()
+exitPrices = c()
+exitDays = c()
+stopPrices = c()
+stopDays = c()
+
+for(i in 2:length(close)){
+  
+  block = 0
+  #Enter
+  if(inTrade == 0 && vix[i-1]/vxv[i-1] < trigger){
+    enterPrice = open[i] - open[i]*slip
+    stopPrice = enterPrice + enterPrice*sl
+    profitPrice = enterPrice - enterPrice*pt
+    inTrade = 1
+    enter = 0
+    block = 1
+    
+    nShares = (runEquity[i-1]*rpt)/(stopPrice-enterPrice)
+    saveEquity = runEquity[i-1]-nShares*enterPrice
+    enterPrices = c(enterPrices,enterPrice)
+    enterDays = c(enterDays,i)
+    stopPrices = c(stopPrices,stopPrice)
+    stopDays = c(stopDays,i)
+  }
+  
+  
+  #Exit Stop Loss
+  if(inTrade == 1 && open[i] > stopPrice && block == 0){
+    exitPrice = open[i] + open[i]*slip
+    inTrade = 0
+    returns = c(returns,(enterPrice-exitPrice)/enterPrice)
+    
+    runEquity[i-1] = nShares*(2*enterPrice - exitPrice) + saveEquity
+    exitPrices = c(exitPrices,exitPrice)
+    exitDays = c(exitDays,i)
+  }else if(inTrade == 1 && high[i] > stopPrice && block == 0){
+    exitPrice = stopPrice + stopPrice*slip
+    inTrade = 0
+    returns = c(returns,(enterPrice-exitPrice)/enterPrice)
+    
+    runEquity[i-1] = nShares*(2*enterPrice - exitPrice) + saveEquity
+    exitPrices = c(exitPrices,exitPrice)
+    exitDays = c(exitDays,i)
+  }
+  
+  #Exit Profit Target
+  if(inTrade == 1 && open[i] < profitPrice && block == 0){
+    exitPrice = open[i] + open[i]*slip
+    inTrade = 0
+    returns = c(returns,(enterPrice-exitPrice)/enterPrice)
+    
+    runEquity[i-1] = nShares*(2*enterPrice-exitPrice) + saveEquity
+    exitPrices = c(exitPrices,exitPrice)
+    exitDays= c(exitDays,i)
+  }else if(inTrade == 1 && low[i] < profitPrice && block == 0){
+    exitPrice = profitPrice + profitPrice*slip
+    inTrade = 0
+    returns = c(returns,(enterPrice-exitPrice)/enterPrice)
+    
+    runEquity[i-1] = nShares*(2*enterPrice - exitPrice) + saveEquity
+    exitPrices = c(exitPrices,exitPrice)
+    exitDays= c(exitDays,i)
+  }
+  
+  #Update Stop
+  if(inTrade == 1 && (close[i]+close[i]*sl) < stopPrice){
+    stopPrice = close[i]+close[i]*sl
+    stopPrices = c(stopPrices,stopPrice)
+    stopDays = c(stopDays,i)
+  }
+  
+  #Update Equity
+  if(inTrade == 0){
+    runEquity[i] = runEquity[i-1]
+  }else{
+    runEquity[i] = nShares*(2*enterPrice-close[i]) + saveEquity
+  }
+  
+}
+
+output[z,1] = (runEquity[length(runEquity)]/runEquity[1])^(1/(length(runEquity)/250))
+output[z,2:5] = c(sl,pt,rpt,trigger)
+}
+
+x = output[!is.na(output[,1]),1]
+#hist(x)
+
